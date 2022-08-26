cd963cafdb83b79e164589ba8c1bc58e7766f7f1
Budi <budiskom99@gmail.com>
2022-08-26 11:13:40 +0700

    Redux: Change Cart

--- src/redux/product/action.js
+++ src/redux/product/action.js
@@ -1,5 +1,6 @@
 export const ADD_TO_CART = 'ADD_TO_CART';
 export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+export const CHANGE_QUANTITY = 'CHANGE_QUANTITY';
 
 export const addToCart = (data) => {
     return {
@@ -11,6 +12,12 @@ export const addToCart = (data) => {
 export const removeProduct = (data) => {
     return {
         type: REMOVE_FROM_CART,
+        value: data,
+    };
+};
+export const changeQty = (data) => {
+    return {
+        type: CHANGE_QUANTITY,
         value: data,
     };
 };
--- src/redux/product/reducer.js
+++ src/redux/product/reducer.js
@@ -1,4 +1,4 @@
-import { ADD_TO_CART, REMOVE_FROM_CART } from './action';
+import { ADD_TO_CART, CHANGE_QUANTITY, REMOVE_FROM_CART } from './action';
 
 const INITIAL_STATE = {
     data: [],
@@ -15,6 +15,14 @@ const reducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state,
                 data: state.data.filter((item) => item !== action.value),
+            };
+        case CHANGE_QUANTITY:
+            return {
+                data: state.data.map((content) =>
+                    content.id === action.id
+                        ? { ...content, qty: action.value }
+                        : content
+                ),
             };
         default:
             return state;
